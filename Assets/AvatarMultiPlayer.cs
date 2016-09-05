b313b5f1ef2dc2923ceb1b2773b4188fb2310298
pese2912 <pese2912@naver.com>
2016-09-05 14:47:46 +0900

    네트워크 매니저를 이용한 네트워킹 실습

---
+++ Assets/AvatarMultiPlayer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class AvatarMultiPlayer : NetworkBehaviour {
+
+    // 로컬 플레이어가 스폰될때만 카메라를 상속받게 하기를 원하기 때문이다.
+    public override void OnStartLocalPlayer()
+    {
+        base.OnStartLocalPlayer();
+        GameObject camera = GameObject.FindWithTag("MainCamera");
+        transform.parent = camera.transform;
+        transform.localPosition = Vector3.zero;
+    }
+}
